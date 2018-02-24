c3a63167eb9c407591b0b1d96c81b66b7576aaa4
DESKTOP-BU5I4BB\hedgehog <hedgehog_fogs@mail.ru>
2018-02-24 22:08:44 +0300

    Reg|Login|CheckDebug

--- src/main/java/kh/com/camarket/controller/RegisterController.java
+++ src/main/java/kh/com/camarket/controller/RegisterController.java
@@ -55,6 +55,7 @@ public class RegisterController {
             }
         }
 
+        System.out.println("REGGGG");
         customer.setEnabled(true);
 
         customerService.addCustomer(customer);
